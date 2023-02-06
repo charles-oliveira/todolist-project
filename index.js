2751ffc6a6afabfe0a906c3e6c7e21a65468ec49
Charles Oliveira <ch4rle2.fh@gmail.com>
2023-02-06 19:32:32 -0300

    add remove from localStorage

--- index.js
+++ index.js
@@ -1,74 +1,60 @@
-const inputField = document.querySelector('input')
-const pushBtn = document.querySelector('#push')
-const tasks = document.querySelector('#tasks')
+const inputField = document.querySelector('input');
+const pushBtn = document.querySelector('#push');
+const tasksContainer = document.querySelector('#tasks');
 
-document.addEventListener("DOMContentLoaded", getTodos)
-
+document.addEventListener('DOMContentLoaded', getTodos);
 
 pushBtn.addEventListener('click', () => {
-    if (inputField.value.length == 0) {
-        alert('Please Enter a Task! ✌🏼')
-    } else {
-       tasks.innerHTML +=  `
-            <div class="task">
-                <span id="taskName">
-                    ${inputField.value}
-                </span>
-                <button class="delete">
-                    <i class="fa-regular fa-trash-can"></i>
-                </button>
-            </div>
-       `
+  if (!inputField.value) {
+    alert('Please enter a task!');
+    return;
+  }
 
-       saveToLocalTodos(inputField.value)
-
-       const currentTask = document.querySelectorAll('.delete')
-        for(let i = 0; i < currentTask.length; i++) {
-            currentTask[i].onclick = function() {
-                this.parentNode.remove()
-            }
-        }
-
-        const crossingOut = document.querySelectorAll('.task')
-        for(let i = 0; i < crossingOut.length; i++) {
-            crossingOut[i].onclick = function() {
-                this.classList.toggle('completed')
-            }
-        }
-
-    }
-    inputField.value = ''
-})
-
-function saveToLocalTodos(todo) {
-    let todos
-    if (localStorage.getItem('todos') === null) {
-        todos = []
-    } else {
-        todos = JSON.parse(localStorage.getItem('todos'))
-    }
-    todos.push(todo)
-    localStorage.setItem('todos', JSON.stringify(todos))
-}
+  const task = createTask(inputField.value);
+  tasksContainer.appendChild(task);
+  saveToLocalTodos(inputField.value);
+  inputField.value = '';
+});
 
 function getTodos() {
-    let todos
-    if (localStorage.getItem('todos') === null) {
-        todos = []
-    } else {
-        todos = JSON.parse(localStorage.getItem('todos'))
-    }
-    todos.forEach(function (todo) {
-        tasks.innerHTML +=  `
-            <div class="task">
-                <span id="taskName">
-                    ${todo}
-                </span>
-                <button class="delete">
-                    <i class="fa-regular fa-trash-can"></i>
-                </button>
-            </div>
-        `
-    })
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
+  todos.forEach(todo => tasksContainer.appendChild(createTask(todo)));
 }
 
+function saveToLocalTodos(todo) {
+  let todos = JSON.parse(localStorage.getItem('todos')) || [];
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function createTask(taskName) {
+  const task = document.createElement('div');
+  task.classList.add('task');
+
+  const taskText = document.createElement('span');
+  taskText.id = 'taskName';
+  taskText.textContent = taskName;
+  task.appendChild(taskText);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete');
+  deleteBtn.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+  deleteBtn.addEventListener('click', () => {
+    removeFromLocalTodos(taskName);
+    task.remove();
+  });
+  task.appendChild(deleteBtn);
+
+  task.addEventListener('click', () => {
+    task.classList.toggle('completed');
+  });
+
+  return task;
+}
+
+function removeFromLocalTodos(taskName) {
+  let todos = JSON.parse(localStorage.getItem('todos')) || [];
+  const taskIndex = todos.indexOf(taskName);
+  todos.splice(taskIndex, 1);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
